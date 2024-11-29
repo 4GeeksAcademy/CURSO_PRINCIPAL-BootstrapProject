6a36b217b3527f5dd0eb9ad8df061b7f1ff48261
AngelDPL <angeld0606@gmail.com>
2024-11-29 14:11:35 +0000

    ejercicio resuelto

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-let distributionRule = document.getElementById("distributionRule");
+let buttonCuadricula = document.getElementById("buttonCuadricula");
+let buttonIndividual = document.getElementById("buttonIndividual");
 
-const newDistribution = 
 
-function changeDistribution () {
-    distributionRule = 
-}
+buttonCuadricula.addEventListener('click', function() {
+    var distributionRule2 = document.getElementById("distributionRule2");
+    distributionRule2.classList.remove('d-flex')
+    distributionRule2.classList.add('d-none')
+
+    var distributionRule1 = document.getElementById("distributionRule1");
+    distributionRule1.classList.remove('d-none')
+    distributionRule1.classList.add('d-flex');
+});
+
+buttonIndividual.addEventListener('click', function() {
+    var  distributionRule1 = document.getElementById("distributionRule1");
+    distributionRule1.classList.remove('d-flex')
+    distributionRule1.classList.add('d-none')
+
+    var distributionRule2 = document.getElementById("distributionRule2");
+    distributionRule2.classList.remove('d-none')
+    distributionRule2.classList.add('d-flex', 'flex-column');
+});
